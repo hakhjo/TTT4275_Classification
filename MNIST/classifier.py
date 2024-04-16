756f14be0d7f4e4506cfa5542c0f4488de7a3275
AFNordal <nordalrasmus01@gmail.com>
2024-04-16 14:59:07 +0200

    blup

--- MNIST/classifier.py
+++ MNIST/classifier.py
@@ -1,9 +1,12 @@
 import numpy as np
+from tqdm import tqdm
 
 class NN:
-    def __init__(self, template_x, template_y):
+    def __init__(self, template_x, template_y, C, D):
         self.template_x = template_x
         self.template_y = template_y
+        self.C = C
+        self.D = D
         self.M = len(template_x)
 
     def evaluate(self, x):
@@ -12,16 +15,19 @@ class NN:
         return self.template_y[idx]
 
     def confusion(self, x, y):
+        errors = 0
         conf = np.zeros((self.C, self.C))
-        for xk, yk in zip(x, y):
+        for xk, yk in zip(tqdm(x), y):
             guess = self.evaluate(xk)
-            conf[yk, guess] += 1
-        return conf
+            conf[yk[0], guess[0]] += 1
+            if guess[0] != yk[0]:
+                errors += 1
+        return conf, errors / len(x)
 
     def validate(self, x, y):
         errors = 0
         for xk, yk in zip(x, y):
             guess = self.evaluate(xk)
-            if guess != yk:
+            if guess[0] != yk[0]:
                 errors += 1
         return errors / len(x)
--- MNIST/main.py
+++ MNIST/main.py
@@ -8,6 +8,15 @@ def display_image(vec):
     plt.show()
 
 train_x, train_y, test_x, test_y = data_loader.load_data()
-c = NN(train_x, train_y)
-print(c.evaluate(test_x[0]), test_y[0])
-display_image(test_x[0])
+# print(train_x.shape)
+p = np.random.permutation(len(train_x))[:1000]
+c = NN(train_x[p], train_y[p], 10, 784)
+test_conf, err = c.confusion(test_x, test_y)
+# train_conf = c.confusion(train_x, train_y)
+test_conf = 100.0 * test_conf / np.sum(test_conf, axis=1)
+# train_conf = 100.0 * train_conf / np.sum(train_conf, axis=1)
+print(f"Error rate: {err*100:.2f}")
+for tr in test_conf:
+    print(" ".join(f"{c:>6.2f}" for c in tr))
+# print(c.evaluate(test_x[0]), test_y[0])
+# display_image(test_x[0])
