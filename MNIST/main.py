464dc8b3c6c0d4994ded64c88987084428c4cf3d
Håkon Hjortdahl <haakohjo@stud.ntnu.no>
2024-04-16 15:55:33 +0200

    kmeans

--- MNIST/main.py
+++ MNIST/main.py
@@ -2,7 +2,9 @@ import numpy as np
 import data_loader
 from matplotlib import pyplot as plt
 from classifier import NN
-
+from sklearn.cluster import KMeans
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
 def display_image(vec):
     plt.imshow(vec.reshape((28, 28)))
     plt.show()
@@ -20,3 +22,34 @@ for tr in test_conf:
     print(" ".join(f"{c:>6.2f}" for c in tr))
 # print(c.evaluate(test_x[0]), test_y[0])
 # display_image(test_x[0])
+M = 64
+kmeans = KMeans(n_clusters=64)
+cluster_centers = np.zeros((10 * M, train_x.shape[1]))
+cluster_labels = np.zeros((10 * M,), dtype=int)
+for i in range(10):
+    indices = np.where(train_y == i)[0]
+    kmeans = KMeans(n_clusters=M, random_state=0)
+    kmeans.fit(train_x[indices])
+    cluster_centers[i*M:(i+1)*M] = kmeans.cluster_centers_
+    cluster_labels[i*M:(i+1)*M] = i   
+
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(cluster_centers, cluster_labels)
+
+test_predictions = knn.predict(test_x)
+nn_conf_matrix = confusion_matrix(test_y, test_predictions)
+nn_error_rate = 1 - accuracy_score(test_y, test_predictions)
+
+print("Conf matrix with N = 1:\n", nn_conf_matrix)
+print("Error rate with N = 1:", nn_error_rate)
+
+knn = KNeighborsClassifier(n_neighbors=7)
+knn.fit(cluster_centers, cluster_labels)
+print("------------------------------------------\n")
+test_predictions = knn.predict(test_x)
+knn_conf_matrix = confusion_matrix(test_y, test_predictions)
+knn_error_rate = 1 - accuracy_score(test_y, test_predictions)
+
+print("Conf Matrix K=7:\n", knn_conf_matrix)
+print("Error Rate K=7):", knn_error_rate)
+
