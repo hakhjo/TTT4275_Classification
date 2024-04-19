56cd6645ad3152374fa7f8ed366716a8cbf52f05
AFNordal <nordalrasmus01@gmail.com>
2024-04-19 14:19:01 +0200

    NN cleanup

--- MNIST/classifier.py
+++ MNIST/classifier.py
@@ -1,43 +1,46 @@
 import numpy as np
-from tqdm import trange
+from tqdm import tqdm, trange
 
 
 class NNClassifier:
-    def __init__(self, template_x, template_y, C, D, chunk_size=1000):
-        self.chunk_size = chunk_size
-        self.n_chunks = int(np.ceil(len(template_x) / chunk_size))
+    def __init__(self, template_x, template_y, chunk_size=None):
+        self.n_templates = len(template_x)
+
         self.template_x = template_x
         self.template_y = template_y
+
+        if chunk_size is None:
+            self.chunk_size = self.n_templates
+        else:
+            self.chunk_size = chunk_size
+        self.n_chunks = int(np.ceil(self.n_templates / self.chunk_size))
         self.template_x_chunked = list(
             [
-                template_x[
-                    i * chunk_size : min(len(template_x), ((i + 1) * chunk_size))
+                self.template_x[
+                    i * self.chunk_size : min(self.n_templates, ((i + 1) * self.chunk_size))
                 ]
                 for i in range(self.n_chunks)
             ]
         )
         self.template_y_chunked = list(
             [
-                template_y[
-                    i * chunk_size : min(len(template_y), ((i + 1) * chunk_size))
+                self.template_y[
+                    i * self.chunk_size : min(self.n_templates, ((i + 1) * self.chunk_size))
                 ]
                 for i in range(self.n_chunks)
             ]
         )
-        self.C = C
-        self.D = D
-        self.M = len(template_x)
 
-    def evaluate_single(self, x):
+    def predict_single(self, x):
         dist = np.sum(np.square(x - self.template_x), axis=1)
         idx = np.argmin(dist)
         return (self.template_y)[idx]
 
-    def evaluate_array(self, x):
+    def predict_chunk(self, x):
         minima = np.zeros((len(x), self.n_chunks))
         minima_labels = np.zeros_like(minima, dtype=int)
         for i in trange(self.n_chunks):
-            diff = x[np.newaxis, :, :] - self.template_x_chunked[i][:, np.newaxis, :]
+            diff = x[:, np.newaxis, :] - self.template_x_chunked[i][np.newaxis, :, :]
             dist = np.sum(np.square(diff), axis=2)
             min_idx = np.argmin(dist, axis=1)
             minima[:, i] = dist[range(len(x)), min_idx]
@@ -46,23 +49,101 @@ class NNClassifier:
         predictions = minima_labels[range(len(x)), glb_min_idx]
         return predictions
 
-    def confusion(self, x, y):
-        errors = 0
-        samples = 0
-        conf = np.zeros((self.C, self.C))
-        for i in trange(len(x)):
-            samples += len(x[i])
-            pred = self.evaluate_array(x[i])
-            for guess, yk in zip(pred, y[i]):
-                conf[yk[0], guess] += 1
-                if guess != yk[0]:
-                    errors += 1
-        return conf, errors / samples
+    def predict_array(self, x, chunk_size=None):
+        N = len(x)
+        if chunk_size is None:
+            chunk_size = N
+        n_chunks = int(np.ceil(N / chunk_size))
+        chunked_x = list(
+            [
+                x[i * chunk_size : min(N, ((i + 1) * chunk_size))]
+                for i in range(n_chunks)
+            ]
+        )
+        predictions = np.hstack([self.predict_chunk(chunk) for chunk in tqdm(chunked_x)])
+        return predictions
 
-    def validate(self, x, y):
-        errors = 0
-        for xk, yk in zip(x, y):
-            guess = self.evaluate(xk)
-            if guess[0] != yk[0]:
-                errors += 1
-        return errors / len(x)
+
+# class KNNClassifier:
+#     def __init__(self, template_x, template_y, n_classes, chunk_size=None):
+#         self.n_classes = n_classes
+#         self.n_templates = len(template_x)
+
+#         self.template_x = template_x
+#         self.template_y = template_y
+
+#         if chunk_size is None:
+#             self.chunk_size = n_classes
+#         else:
+#             self.chunk_size = chunk_size
+#         self.n_chunks = int(np.ceil(self.n_templates / self.chunk_size))
+#         self.template_x_chunked = list(
+#             [
+#                 self.template_x[
+#                     i * self.chunk_size : min(self.n_templates, ((i + 1) * self.chunk_size))
+#                 ]
+#                 for i in range(self.n_chunks)
+#             ]
+#         )
+#         self.template_y_chunked = list(
+#             [
+#                 self.template_y[
+#                     i * self.chunk_size : min(self.n_templates, ((i + 1) * self.chunk_size))
+#                 ]
+#                 for i in range(self.n_chunks)
+#             ]
+#         )
+
+#     def predict_single(self, x):
+#         dist = np.sum(np.square(x - self.template_x), axis=1)
+#         idx = np.argmin(dist)
+#         return (self.template_y)[idx]
+
+#     def predict_chunk(self, x):
+#         minima = np.zeros((len(x), self.n_chunks))
+#         minima_labels = np.zeros_like(minima, dtype=int)
+#         for i in trange(self.n_chunks):
+#             diff = x[np.newaxis, :, :] - self.template_x_chunked[i][:, np.newaxis, :]
+#             dist = np.sum(np.square(diff), axis=2)
+#             min_idx = np.argmin(dist, axis=1)
+#             minima[:, i] = dist[range(len(x)), min_idx]
+#             minima_labels[:, i] = self.template_y_chunked[i][min_idx, 0]
+#         glb_min_idx = np.argmin(minima, axis=1)
+#         predictions = minima_labels[range(len(x)), glb_min_idx]
+#         return predictions
+
+#     def evaluate_array(self, x, chunk_size=None):
+#         N = len(x)
+#         if chunk_size is None:
+#             chunk_size = N
+#         n_chunks = int(np.ceil(N / chunk_size))
+#         chunked_x = list(
+#             [
+#                 x[i * chunk_size : min(N, ((i + 1) * chunk_size))]
+#                 for i in range(n_chunks)
+#             ]
+#         )
+#         predictions = np.hstack([self.predict_chunk(chunk) for chunk in tqdm(chunked_x)])
+#         return predictions
+
+
+#     def confusion(self, x, y):
+#         errors = 0
+#         samples = 0
+#         conf = np.zeros((self.n_classes, self.n_classes))
+#         for i in trange(len(x)):
+#             samples += len(x[i])
+#             pred = self.predict_chunk(x[i])
+#             for guess, yk in zip(pred, y[i]):
+#                 conf[yk[0], guess] += 1
+#                 if guess != yk[0]:
+#                     errors += 1
+#         return conf, errors / samples
+
+#     def validate(self, x, y):
+#         errors = 0
+#         for xk, yk in zip(x, y):
+#             guess = self.evaluate(xk)
+#             if guess[0] != yk[0]:
+#                 errors += 1
+#         return errors / len(x)
--- MNIST/main.py
+++ MNIST/main.py
@@ -1,9 +1,9 @@
 import numpy as np
-import data_loader
+import utils
 import pandas as pd
 import seaborn as sn
 from matplotlib import pyplot as plt
-from classifier import NN
+from classifier import NNClassifier
 from sklearn.cluster import KMeans
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score
@@ -15,89 +15,77 @@ def display_image(vec):
     plt.show()
 
 
-train_x, train_y, test_x, test_y = data_loader.load_data()
+train_x, train_y, test_x, test_y = utils.load_data()
 
-M = 64
-kmeans = KMeans(n_clusters=64)
-cluster_centers = np.zeros((10 * M, train_x.shape[1]))
-cluster_labels = np.zeros((10 * M,), dtype=int)
-centroids = np.zeros((10 * M, train_x.shape[1]))
-for i in range(10):
-    indices = np.where(train_y == i)[0]
-    kmeans = KMeans(n_clusters=M)
-    kmeans.fit(train_x[indices])
-    cluster_centers[i*M:(i+1)*M] = kmeans.cluster_centers_
-    cluster_labels[i*M:(i+1)*M] = i   
+# M = 64
+# kmeans = KMeans(n_clusters=64)
+# cluster_centers = np.zeros((10 * M, train_x.shape[1]))
+# cluster_labels = np.zeros((10 * M,), dtype=int)
+# centroids = np.zeros((10 * M, train_x.shape[1]))
+# for i in range(10):
+#     indices = np.where(train_y == i)[0]
+#     kmeans = KMeans(n_clusters=M)
+#     kmeans.fit(train_x[indices])
+#     cluster_centers[i*M:(i+1)*M] = kmeans.cluster_centers_
+#     cluster_labels[i*M:(i+1)*M] = i
 
-def plot_centroids(cluster_centers):
-    for i in range(10):
-        centroids[i*M:(i+1)*M] = cluster_centers[i*M:(i+1)*M].reshape(M, -1)
-    reshaped_centroids = centroids.reshape((10 * M, 28, 28))
-    for i in range(10):
-        for j in range(M):
-            plt.subplot(10, M, i * M + j + 1)
-            plt.imshow(reshaped_centroids[i * M + j], cmap='gray')
-            plt.axis('off')
-    plt.show()
+# def plot_centroids(cluster_centers):
+#     for i in range(10):
+#         centroids[i*M:(i+1)*M] = cluster_centers[i*M:(i+1)*M].reshape(M, -1)
+#     reshaped_centroids = centroids.reshape((10 * M, 28, 28))
+#     for i in range(10):
+#         for j in range(M):
+#             plt.subplot(10, M, i * M + j + 1)
+#             plt.imshow(reshaped_centroids[i * M + j], cmap='gray')
+#             plt.axis('off')
+#     plt.show()
 
-def KNearestNeighbours(n_neighbors):
-    knn = KNeighborsClassifier(n_neighbors=n_neighbors)
-    
-    knn.fit(cluster_centers, cluster_labels)
-    start = time.time()
-    test_predictions = knn.predict(test_x)
-    end = time.time()
-    print(f"Time used NN with {n_neighbors}: {end - start}")
-    training_predictions = knn.predict(train_x)
+# def KNearestNeighbours(n_neighbors):
+#     knn = KNeighborsClassifier(n_neighbors=n_neighbors)
 
-    nn_conf_matrix_train = confusion_matrix(train_y, training_predictions)
-    nn_error_rate_train= 1 - accuracy_score(train_y, training_predictions)
-    print(f" train Conf matrix with K = {n_neighbors}:\n {nn_conf_matrix_train}")
-    print(f"train Error rate with K = {n_neighbors}:{nn_error_rate_train}")
-    nn_conf_matrix = confusion_matrix(test_y, test_predictions)
-    nn_error_rate = 1 - accuracy_score(test_y, test_predictions)
-    plot_confusion_matrix(f"Confusion Matrix with N = {n_neighbors}", nn_conf_matrix)
+#     knn.fit(cluster_centers, cluster_labels)
+#     start = time.time()
+#     test_predictions = knn.predict(test_x)
+#     end = time.time()
+#     print(f"Time used NN with {n_neighbors}: {end - start}")
+#     training_predictions = knn.predict(train_x)
 
-    # print(f"Conf matrix with K = {n_neighbors}:\n {nn_conf_matrix}")
-    print(f"Error rate with K = {n_neighbors} {nn_error_rate}")
+#     nn_conf_matrix_train = confusion_matrix(train_y, training_predictions)
+#     nn_error_rate_train= 1 - accuracy_score(train_y, training_predictions)
+#     print(f" train Conf matrix with K = {n_neighbors}:\n {nn_conf_matrix_train}")
+#     print(f"train Error rate with K = {n_neighbors}:{nn_error_rate_train}")
+#     nn_conf_matrix = confusion_matrix(test_y, test_predictions)
+#     nn_error_rate = 1 - accuracy_score(test_y, test_predictions)
+#     plot_confusion_matrix(f"Confusion Matrix with N = {n_neighbors}", nn_conf_matrix)
 
-    return end -start
+#     # print(f"Conf matrix with K = {n_neighbors}:\n {nn_conf_matrix}")
+#     print(f"Error rate with K = {n_neighbors} {nn_error_rate}")
 
-def plot_confusion_matrix(name, conf_mat):
-    df_cm = pd.DataFrame(conf_mat, index = [i for i in "0123456789"],columns = [i for i in "0123456789"])
-    fig = plt.figure(figsize = (10,7))
+#     return end -start
 
-    fig.suptitle(name, fontsize=16)
+# def plot_confusion_matrix(name, conf_mat):
+#     df_cm = pd.DataFrame(conf_mat, index = [i for i in "0123456789"],columns = [i for i in "0123456789"])
+#     fig = plt.figure(figsize = (10,7))
 
-    sn.heatmap(df_cm, annot=True)
-    plt.show()
-# plot_centroids(cluster_centers)
-time_1 = KNearestNeighbours(1)
-time_7 = KNearestNeighbours(7)
-print("increas in time:", time_7/time_1)
+#     fig.suptitle(name, fontsize=16)
 
+#     sn.heatmap(df_cm, annot=True)
+#     plt.show()
+# # plot_centroids(cluster_centers)
+# time_1 = KNearestNeighbours(1)
+# time_7 = KNearestNeighbours(7)
+# print("increas in time:", time_7/time_1)
 
 
 chunk_size = 1000
-n_chunks = int(np.ceil(len(test_x) / chunk_size))
-chunked_test_x = list(
-    [
-        test_x[i * chunk_size : min(len(test_x), ((i + 1) * chunk_size))]
-        for i in range(n_chunks)
-    ]
-)
-chunked_test_y = list(
-    [
-        test_y[i * chunk_size : min(len(test_y), ((i + 1) * chunk_size))]
-        for i in range(n_chunks)
-    ]
-)
+c = NNClassifier(train_x[:10000], train_y[:10000], chunk_size=chunk_size)
+pred = c.predict_array(test_x[:100], chunk_size=100)
+np.savetxt("test_predicitions.txt", pred)
+test_conf = utils.confusion_matrix(pred, test_y[:100, 0], 10, relative=False)
+utils.print_mat(test_conf)
+# test_conf, err = c.confusion(chunked_test_x, chunked_test_y)
 
-
-c = NN(train_x, train_y, 10, 784)
-test_conf, err = c.confusion(chunked_test_x, chunked_test_y)
-
-test_conf = 100.0 * test_conf / np.sum(test_conf, axis=1)
-print(f"Error rate: {err*100:.2f}")
-for tr in test_conf:
-    print(" ".join(f"{c:>6.2f}" for c in tr))
+# test_conf = 100.0 * test_conf / np.sum(test_conf, axis=1)
+# print(f"Error rate: {err*100:.2f}")
+# for tr in test_conf:
+#     print(" ".join(f"{c:>6.2f}" for c in tr))
