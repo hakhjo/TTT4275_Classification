01d58445806bf66522739f34bc7f6a1a1b886fcf
AFNordal <nordalrasmus01@gmail.com>
2024-04-19 10:59:09 +0200

    not working

--- MNIST/classifier.py
+++ MNIST/classifier.py
@@ -1,11 +1,11 @@
 import numpy as np
-from tqdm import tqdm
+from tqdm import tqdm, trange
 
 
 class NN:
     def __init__(self, template_x, template_y, C, D, chunk_size=1000):
         self.chunk_size = chunk_size
-        self.n_chunks = int(np.floor(len(template_x) / chunk_size))
+        self.n_chunks = int(np.ceil(len(template_x) / chunk_size))
         self.template_x = list(
             [
                 template_x[i * chunk_size : min(len(template_x), ((i + 1) * chunk_size))]
@@ -22,29 +22,38 @@ class NN:
         self.D = D
         self.M = len(template_x)
 
-    def evaluate(self, x):
-        dist = np.sum(np.square(x - self.template_x), axis=1)
+    def evaluate_single(self, x):
+        dist = np.sum(np.square(x - np.vstack(self.template_x)), axis=1)
         idx = np.argmin(dist)
-        return self.template_y[idx]
+        return np.vstack(self.template_y)[idx]
 
-    def evaluate2(self, x):
-        minima = np.zeros_like(self.n_chunks)
-        for i in range(self.n_chunks):
+    def evaluate_array(self, x):
+        minima = np.zeros((len(x), self.n_chunks))
+        minima_labels = np.zeros_like(minima, dtype=int)
+        for i in tqdm(range(self.n_chunks)):
             x_tiled = np.transpose(np.tile(x, (self.chunk_size, 1, 1)), (1, 0, 2))
             temp_tiled = np.tile(self.template_x[i], (x.shape[0], 1, 1))
             dist = np.sum(np.square(x_tiled-temp_tiled), axis=2)
-
-        return
+            min_idx = np.argmin(dist, axis=1)
+            minima[:, i] = dist[range(len(x)), min_idx]
+            minima_labels[:, i] = self.template_y[i][min_idx, 0]
+            # print(dist[range(len(x)), min_idx].shape)
+        glb_min_idx = np.argmin(minima, axis=1)
+        predictions = minima_labels[range(len(x)), glb_min_idx]
+        return predictions
 
     def confusion(self, x, y):
         errors = 0
+        samples = 0
         conf = np.zeros((self.C, self.C))
-        for xk, yk in zip(tqdm(x), y):
-            guess = self.evaluate(xk)
-            conf[yk[0], guess[0]] += 1
-            if guess[0] != yk[0]:
-                errors += 1
-        return conf, errors / len(x)
+        for i in trange(len(x)):
+            samples += len(x[i])
+            pred = self.evaluate_array(x[i])
+            for guess, yk in zip(pred, y[i]):
+                conf[yk[0], guess] += 1
+                if guess != yk[0]:
+                    errors += 1
+        return conf, errors / samples
 
     def validate(self, x, y):
         errors = 0
--- MNIST/main.py
+++ MNIST/main.py
@@ -2,18 +2,41 @@ import numpy as np
 import data_loader
 from matplotlib import pyplot as plt
 from classifier import NN
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+
 
 def display_image(vec):
     plt.imshow(vec.reshape((28, 28)))
     plt.show()
 
+
 train_x, train_y, test_x, test_y = data_loader.load_data()
 
+chunk_size = 1000
+n_chunks = int(np.ceil(len(test_x) / chunk_size))
+chunked_test_x = list(
+    [
+        test_x[i * chunk_size : min(len(test_x), ((i + 1) * chunk_size))]
+        for i in range(n_chunks)
+    ]
+)
+chunked_test_y = list(
+    [
+        test_y[i * chunk_size : min(len(test_y), ((i + 1) * chunk_size))]
+        for i in range(n_chunks)
+    ]
+)
+
+c1 = KNeighborsClassifier(n_neighbors=1)
+c1.fit(train_x, train_y)
+pred = c1.predict(test_x)
+print(confusion_matrix(test_y, pred))
+print(1-accuracy_score(test_y, pred))
+
 c = NN(train_x, train_y, 10, 784)
-p = np.random.permutation(len(test_x))[:1000]
-c.evaluate2(test_x[p])
-exit()
-test_conf, err = c.confusion(test_x, test_y)
+test_conf, err = c.confusion(chunked_test_x, chunked_test_y)
+
 test_conf = 100.0 * test_conf / np.sum(test_conf, axis=1)
 print(f"Error rate: {err*100:.2f}")
 for tr in test_conf:
