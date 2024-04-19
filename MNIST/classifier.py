f42925b82c580ea9d7f69b38a8e66c554fca15a3
AFNordal <nordalrasmus01@gmail.com>
2024-04-19 09:19:11 +0200

    init

--- MNIST/classifier.py
+++ MNIST/classifier.py
@@ -1,18 +1,40 @@
 import numpy as np
 from tqdm import tqdm
 
+
 class NN:
-    def __init__(self, template_x, template_y, C, D):
-        self.template_x = template_x
-        self.template_y = template_y
+    def __init__(self, template_x, template_y, C, D, chunk_size=1000):
+        self.chunk_size = chunk_size
+        self.n_chunks = int(np.floor(len(template_x) / chunk_size))
+        self.template_x = list(
+            [
+                template_x[i * chunk_size : min(len(template_x), ((i + 1) * chunk_size))]
+                for i in range(self.n_chunks)
+            ]
+        )
+        self.template_y = list(
+            [
+                template_y[i * chunk_size : min(len(template_y), ((i + 1) * chunk_size))]
+                for i in range(self.n_chunks)
+            ]
+        )
         self.C = C
         self.D = D
         self.M = len(template_x)
 
     def evaluate(self, x):
-        dist = np.sum(np.square(x-self.template_x), axis=1)
+        dist = np.sum(np.square(x - self.template_x), axis=1)
         idx = np.argmin(dist)
         return self.template_y[idx]
+
+    def evaluate2(self, x):
+        minima = np.zeros_like(self.n_chunks)
+        for i in range(self.n_chunks):
+            x_tiled = np.transpose(np.tile(x, (self.chunk_size, 1, 1)), (1, 0, 2))
+            temp_tiled = np.tile(self.template_x[i], (x.shape[0], 1, 1))
+            dist = np.sum(np.square(x_tiled-temp_tiled), axis=2)
+
+        return
 
     def confusion(self, x, y):
         errors = 0
