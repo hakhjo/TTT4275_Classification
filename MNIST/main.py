54670969326aecefcf3886425e6b97b3e6b9edbd
AFNordal <nordalrasmus01@gmail.com>
2024-04-22 15:35:08 +0200

    confidence intervals

--- MNIST/main.py
+++ MNIST/main.py
@@ -1,8 +1,11 @@
 import numpy as np
+from tqdm import trange
 import utils
 from matplotlib import pyplot as plt
 from classifier import NNClassifier, ClusteredKNNClassifier
 import time
+from sklearn.neighbors import KNeighborsClassifier
+import pickle
 
 np.random.seed(0)
 
@@ -30,19 +33,39 @@ def run_NN_timed(c: NNClassifier):
     return res
 
 
+# res = {"err":[], "K":[]}
+# for K in range(1, 21):
+#     c = KNeighborsClassifier(n_neighbors=K)
+#     c.fit(train_x, train_y[:, 0])
+#     guesses = c.predict(test_x)
+#     err = utils.error_rate(guesses, test_y[:, 0])
+#     res["err"].append(err)
+#     res["K"].append(K)
+#     print(K, err)
+
+# with open("K-test", "wb") as f:
+#     pickle.dump(res, f)
+
+with open("K-test", "rb") as f:
+    res_ = pickle.load(f)
+
+plt.plot(res_["K"], res_["err"])
+# plt.show()
 
 
+# c = NNClassifier(train_x, train_y, chunk_size=1000)
+# run_NN_timed(c)
 
+# start_cluster = time.time()
+# c = ClusteredKNNClassifier(train_x, train_y, N, M)
+# cluster_time = time.time() - start_cluster
 
-c = NNClassifier(train_x, train_y, chunk_size=1000)
-run_NN_timed(c)
+# print(f"Clustering took {cluster_time}s")
 
-start_cluster = time.time()
-c = ClusteredKNNClassifier(train_x, train_y, N, M)
-cluster_time = time.time() - start_cluster
+# run_KNN_timed(c, 1)
+# run_KNN_timed(c, 7)
 
-print(f"Clustering took {cluster_time}s")
-
-run_KNN_timed(c, 1)
-run_KNN_timed(c, 7)
+res = utils.result_from_file("1-NN_cmplt")
+pl, ph = utils.wilson_CI(res.err, len(test_x))
+print(pl, res.err, ph)
 
